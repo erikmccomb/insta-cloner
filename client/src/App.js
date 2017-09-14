60ae540a2f8b1edbd4b4ec9a86463eb00b73fc35
Erik McComb <erik@erikmccomb.com>
2017-09-14 10:14:47 -0600

    Created register.js, gem file fully updated.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import Register from './components/Register';
+
+class App extends Component {
+  render() {
+    return (
+      </Container>
+        <Register />
+      </Container>
+    );
+  }
+}
+
+export default App;
